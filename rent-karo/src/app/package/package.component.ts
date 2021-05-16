9f3ff163ee5da51f6af141cac2f9dad24ebeb51e
Vikas Saraf <saraf.vikas5@gmail.com>
2021-05-16 23:35:15 +0530

    added package component

--- rent-karo/src/app/package/package.component.ts
+++ rent-karo/src/app/package/package.component.ts
@@ -7,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./package.component.css']
 })
 export class PackageComponent implements OnInit {
+  showPack = false;
+  showMonthlyPack = false;
+  showYearlyPack = false;
+  packageNames = ['Weekly', 'Monthly', 'Yearly'];
+  packageName;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showWeeklyPackage() {
+    this.showPack = true;
+    this.packageName = this.packageNames[0];
+  }
+
+  showMonthlyPackage() {
+    this.showPack = true;
+    this.packageName = this.packageNames[1];
+  }
+
+  showYearlyPackage() {
+    this.showPack = true;
+    this.packageName = this.packageNames[2];
   }
 
 }
