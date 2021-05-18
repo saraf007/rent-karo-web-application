f7befe901a18a67ea51a8e2e9d793e8f81128380
Vikas Saraf <saraf.vikas5@gmail.com>
2021-05-18 09:26:45 +0530

    modified package component

--- rent-karo/src/app/package/package.component.ts
+++ rent-karo/src/app/package/package.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PackageComponent implements OnInit {
   showPack = false;
-  showMonthlyPack = false;
-  showYearlyPack = false;
   packageNames = ['Weekly', 'Monthly', 'Yearly'];
   packageName;
+  weeklyPackagePrices = [10000, 6000, 4000];
+  weeklyPackagePrice;
+  isMonthly = false;
+  isPremium = false;
+  isWeeklyPackage = false;
+  isWeeklyStandard = false;
+  premiumPrice;
+  standardPrice;
+  basicPrice;
+  packageDuration;
 
   constructor() { }
 
@@ -21,16 +29,28 @@ export class PackageComponent implements OnInit {
   showWeeklyPackage() {
     this.showPack = true;
     this.packageName = this.packageNames[0];
+    this.premiumPrice = 10000;
+    this.standardPrice = 7000;
+    this.basicPrice = 5000;
+    this.packageDuration = "week";
   }
 
   showMonthlyPackage() {
     this.showPack = true;
     this.packageName = this.packageNames[1];
+    this.premiumPrice = 7000;
+    this.standardPrice = 5000;
+    this.basicPrice = 3000;
+    this.packageDuration = "month";
   }
 
   showYearlyPackage() {
     this.showPack = true;
     this.packageName = this.packageNames[2];
+    this.premiumPrice = 5000;
+    this.standardPrice = 3000;
+    this.basicPrice = 2000;
+    this.packageDuration = "year";
   }
 
 }
